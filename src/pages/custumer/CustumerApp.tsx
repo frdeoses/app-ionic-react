cc217225ab689666ce56082667d4bd4cfde12479
frdeoses <fdoses@outlook.es>
2022-01-06 13:16:25 +0100

    implements functions

--- src/pages/custumer/CustumerApp.tsx
+++ src/pages/custumer/CustumerApp.tsx
@@ -1,5 +1,45 @@
-export function searchCustumer() {}
+export function searchCustumers() {
+  if (!localStorage["custumers"]) {
+    localStorage["custumers"] = "[]";
+  }
+  let custumers = localStorage["custumers"];
 
-export function removeCustumer() {}
+  custumers = JSON.parse(custumers);
 
-export function saveCustumer() {}
+  return custumers;
+  //   const datosEjemplo = [
+  //     {
+  //       id: "1",
+  //       firstName: "Fran",
+  //       lastName: "boni",
+  //       email: "fran@test.com",
+  //       telf: "658741527",
+  //       address: "C/ pastillas",
+  //     },
+  //     {
+  //       id: "2",
+  //       firstName: "Maria",
+  //       lastName: "Castro",
+  //       email: "maria@test.com",
+  //       telf: "547851234",
+  //       address: "C/ torre",
+  //     },
+  //   ];
+}
+
+export function removeCustumer(id: string) {
+  let custumers = searchCustumers();
+
+  let indice = custumers.findIndex((custumer: any) => custumer.id == id);
+
+  custumers.splice(indice, 1);
+
+  localStorage["custumers"] = JSON.stringify(custumers);
+}
+
+export function saveCustumer(custumer: any) {
+  let custumers = searchCustumers();
+  custumers.push(custumer);
+
+  localStorage["custumers"] = JSON.stringify(custumers);
+}
