8734384fe58d5ea2594cfab184daa8f435a43636
frdeoses <fdoses@outlook.es>
2022-01-08 20:34:03 +0100

    Create employee

---
+++ src/pages/employee/EmployeeEdit.tsx
@@ -0,0 +1,169 @@
+import {
+  IonButton,
+  IonButtons,
+  IonCard,
+  IonCol,
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonMenuButton,
+  IonPage,
+  IonRow,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import { checkmark } from "ionicons/icons";
+import { useEffect, useState } from "react";
+import { useHistory, useParams } from "react-router";
+import Employee from "./Employee";
+import { saveEmployee, searchEmployeeById } from "./EmployeeApp";
+
+const EmployeeEdit: React.FC = () => {
+  const { name, id } = useParams<{ name: string; id: string }>();
+  const [employee, setEmployee] = useState<Employee>({});
+  const history = useHistory();
+
+  useEffect(() => {
+    search();
+  }, []);
+
+  const search = () => {
+    debugger;
+    if (id !== "new") {
+      let res = searchEmployeeById(id);
+      setEmployee(res);
+    }
+    // let res = searchEmployees();
+    // setClientes(res);
+  };
+  const save = () => {
+    saveEmployee(employee);
+    history.push("/page/employees");
+  };
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonMenuButton />
+          </IonButtons>
+          <IonTitle>{name}</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">{name}</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+
+        <IonContent>
+          <IonCard>
+            <IonTitle>
+              {" "}
+              {id === "new" ? "Agregar Empleado" : "Editar Empleado"}
+            </IonTitle>
+
+            <IonRow>
+              <IonCol>
+                <IonItem>
+                  <IonLabel position="stacked">Nombre</IonLabel>
+                  <IonInput
+                    onIonChange={(e) => {
+                      employee.firstName = String(e.detail.value);
+                    }}
+                    value={employee.firstName}
+                  >
+                    {" "}
+                  </IonInput>
+                </IonItem>
+              </IonCol>
+
+              <IonCol>
+                <IonItem>
+                  <IonLabel position="stacked">Apellidos</IonLabel>
+                  <IonInput
+                    onIonChange={(e) => {
+                      employee.lastName = String(e.detail.value);
+                    }}
+                    value={employee.lastName}
+                  >
+                    {" "}
+                  </IonInput>
+                </IonItem>
+              </IonCol>
+            </IonRow>
+
+            <IonRow>
+              <IonCol>
+                <IonItem>
+                  <IonLabel position="stacked">Email</IonLabel>
+                  <IonInput
+                    onIonChange={(e) => {
+                      employee.email = String(e.detail.value);
+                    }}
+                    value={employee.email}
+                  >
+                    {" "}
+                  </IonInput>
+                </IonItem>
+              </IonCol>
+
+              <IonCol>
+                <IonItem>
+                  <IonLabel position="stacked">Addres</IonLabel>
+                  <IonInput
+                    onIonChange={(e) => {
+                      employee.address = String(e.detail.value);
+                    }}
+                    value={employee.address}
+                  >
+                    {" "}
+                  </IonInput>
+                </IonItem>
+              </IonCol>
+            </IonRow>
+
+            <IonRow>
+              <IonCol>
+                <IonItem>
+                  <IonLabel position="stacked">Telf</IonLabel>
+                  <IonInput
+                    onIonChange={(e) => {
+                      employee.telf = String(e.detail.value);
+                    }}
+                    value={employee.telf}
+                  >
+                    {" "}
+                  </IonInput>
+                </IonItem>
+              </IonCol>
+
+              <IonCol></IonCol>
+            </IonRow>
+
+            <IonItem>
+              <IonButton
+                onClick={save}
+                color="success"
+                fill="solid"
+                slot="end"
+                size="default"
+              >
+                <IonIcon icon={checkmark} />
+                Guardar
+              </IonButton>
+            </IonItem>
+          </IonCard>
+        </IonContent>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default EmployeeEdit;
---
+++ src/pages/employee/EmployeeList.tsx
@@ -0,0 +1,128 @@
+import {
+  IonButton,
+  IonButtons,
+  IonCard,
+  IonCol,
+  IonContent,
+  IonGrid,
+  IonHeader,
+  IonIcon,
+  IonItem,
+  IonMenuButton,
+  IonPage,
+  IonRow,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import { add, close, pencil } from "ionicons/icons";
+import { useEffect, useState } from "react";
+import { useHistory, useParams } from "react-router";
+import Employee from "./Employee";
+import { removeEmployee, searchEmployees } from "./EmployeeApp";
+
+const EmployeeList: React.FC = () => {
+  const { name } = useParams<{ name: string }>();
+  const [clientes, setClientes] = useState<Employee[]>([]);
+  const history = useHistory();
+
+  useEffect(() => {
+    search();
+  }, [history.location.pathname]);
+
+  const remove = (id: string) => {
+    removeEmployee(id);
+    search();
+  };
+
+  const search = () => {
+    let res = searchEmployees();
+    setClientes(res);
+  };
+
+  const addEmployee = () => {
+    history.push("/page/employee/new");
+  };
+  const editEmployee = (id: string) => {
+    debugger;
+    history.push("/page/employee/" + id);
+  };
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonMenuButton />
+          </IonButtons>
+          <IonTitle>{name}</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">{name}</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+
+        <IonContent>
+          <IonCard>
+            <IonTitle>Gestión de Empleados</IonTitle>
+
+            <IonItem>
+              <IonButton
+                onClick={addEmployee}
+                color="primary"
+                fill="solid"
+                slot="end"
+                size="default"
+              >
+                <IonIcon icon={add} />
+                Agregar Empleado
+              </IonButton>
+            </IonItem>
+
+            <IonGrid className="table">
+              <IonRow>
+                <IonCol>Nombre</IonCol>
+                <IonCol>Email</IonCol>
+                <IonCol>Teléfono</IonCol>
+                <IonCol>Dirección</IonCol>
+                <IonCol>Acciones</IonCol>
+              </IonRow>
+
+              {clientes.map((cliente: Employee) => (
+                <IonRow>
+                  <IonCol>
+                    {cliente.firstName} {cliente.lastName}
+                  </IonCol>
+                  <IonCol>{cliente.email}</IonCol>
+                  <IonCol>{cliente.telf}</IonCol>
+                  <IonCol>{cliente.address}</IonCol>
+                  <IonCol>
+                    <IonButton
+                      onClick={() => editEmployee(String(cliente.id))}
+                      color="primary"
+                      fill="clear"
+                    >
+                      <IonIcon icon={pencil} slot="icon-only"></IonIcon>
+                    </IonButton>
+                    <IonButton
+                      onClick={() => remove(String(cliente.id))}
+                      color="danger"
+                      fill="clear"
+                    >
+                      <IonIcon icon={close} slot="icon-only"></IonIcon>
+                    </IonButton>
+                  </IonCol>
+                </IonRow>
+              ))}
+            </IonGrid>
+          </IonCard>
+        </IonContent>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default EmployeeList;
